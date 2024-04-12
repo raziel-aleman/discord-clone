8f5f64639bf0c0359601836097f079355175bb5f
Raziel Aleman <razielali10@gmail.com>
2024-04-11 23:42:47 -0800

    Initial commit of source code

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,71 @@
+import ChatHeader from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        },
+    });
+
+    if (!channel || !member) {
+        redirect("/");
+    }
+
+    return (
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader
+                serverId={channel.serverId}
+                name={channel.name}
+                type="channel"
+            />
+            <ChatMessages
+                name={channel.name}
+                member={member}
+                chatId={channel.id}
+                apiUrl="/api/messages"
+                socketUrl="/api/socket/messages"
+                socketQuery={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+                paramKey="channelId"
+                paramValue={channel.id}
+                type="channel"
+            />
+            <ChatInput
+                apiUrl="/api/socket/messages"
+                query={{ channelId: channel.id, serverId: channel.serverId }}
+                name={channel.name}
+                type="channel"
+            />
+        </div>
+    );
+};
+
+export default ChannelIdPage;
